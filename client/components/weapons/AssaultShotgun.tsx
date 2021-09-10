8024d247d768f7f1c63ee7cab501b83958a357e1
Alex Kuhrt <kuhrt@patronas.com>
2021-09-10 21:36:47 +0200

    more fixes

--- client/components/weapons/AssaultShotgun.tsx
+++ client/components/weapons/AssaultShotgun.tsx
@@ -1,7 +1,7 @@
 export const AssaultShotgun = (): JSX.Element => (
   <svg
     width="100"
-    height="auto"
+    height="100%"
     viewBox="0 0 192 64"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
--- client/components/weapons/Ratio.tsx
+++ client/components/weapons/Ratio.tsx
@@ -1,5 +1,5 @@
 export const Ratio = (): JSX.Element => (
-  <svg width="100" height="auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="100" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <mask
       id="mask0"
       maskUnits="userSpaceOnUse"
